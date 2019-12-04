fdcc7691d24417f06be03c43ae78f20a671e0075
Orivelton Cesar <orivelton10@gmail.com>
2019-12-04 21:34:24 +0000

    chore: store

--- src/store.js
+++ src/store.js
@@ -2,7 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
+import rootReducer from './reducers';
+
 const middleware = [thunk];
 const initialState = {};
 
-const store = createStore();
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
